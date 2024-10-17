1daed6688db9be1eb9c614280b02b11bfc461ccf
Alejandro Pérez Alcojol <138225282+Zero2680@users.noreply.github.com>
2024-10-17 16:45:41 +0200

    Update nuss.py

--- nuss.py
+++ nuss.py
@@ -1,9 +1,11 @@
 from pygame import*
 from random import*
+from threading import*
 w = 1280
 h = 720
 x = 0
 screen = display.set_mode((w, h), FULLSCREEN)
+display.set_caption('NUSS')
 clock = time.Clock()
 done = False
 background = transform.scale(image.load("nuss_images/background_day.png").convert(), (3480, 720))
@@ -20,6 +22,11 @@ class Objeto(sprite.Sprite):
 
 	def DibujarObjeto(self):
 		draw.rect(screen, self.color, (self.x,self.y,self.ancho,self.largo))
+
+class Barra(Objeto):
+	def __init__(self, x, y, ancho, largo, color , vidas):
+		Objeto.__init__(self, x, y, ancho, largo, color, vidas)
+
 
 class Personaje(Objeto):
 	def __init__(self, x, y, ancho, largo, color , vidas):
@@ -111,7 +118,7 @@ class Personaje(Objeto):
 			if timer == 20:
 				timer = 0
 				plus_timer = False
-		self.DibujarObjeto()
+		#self.DibujarObjeto()
 
 	def check_colisiones(sprite1, sprite2):
 		xsprite1 = sprite1.x
@@ -403,14 +410,27 @@ class Disparo(Objeto):
 
 plataforma = Personaje(50, 260, 200, 200,(0,0,0), 3)
 fantasma = transform.scale(image.load("nuss_images/fantasma.png"), (200, 200))
+fantasma2 = transform.scale(image.load("nuss_images/fantasma2.png"), (200, 200))
+fantasma3 = transform.scale(image.load("nuss_images/fantasma3.png"), (200, 200))
 nube = transform.scale(image.load("nuss_images/nube.png"), (600, 600))
 rayo = transform.scale(image.load("nuss_images/rayo.png"), (300, 300))
 sol = transform.scale(image.load("nuss_images/sol.png"), (600, 600))
 fuego = transform.scale(image.load("nuss_images/fuego.png"), (150, 150))
-luna = transform.scale(image.load("nuss_images/nube.png"), (600, 600))
-meteorito = transform.scale(image.load("nuss_images/rayo.png"), (300, 300))
+luna = transform.scale(image.load("nuss_images/luna.png"), (600, 600))
+meteorito1 = transform.scale(image.load("nuss_images/meteorito1.png"), (300, 300))
+meteorito2 = transform.scale(image.load("nuss_images/meteorito2.png"), (300, 300))
+alien = transform.scale(image.load("nuss_images/alien.png"), (600, 600))
+ovni = transform.scale(image.load("nuss_images/ovni.png"), (150, 150))
 boss = Boss(1000, 30, 250, 600, (255,255,255), 1)
+barra = Barra(340, 25, 0, 25,(166, 215, 255), 3)
+barra_bg = transform.scale(image.load("nuss_images/barra_bg.png"), (625, 25))
+nube_icon = transform.scale(image.load("nuss_images/nube_icon.png"), (40, 40))
+sol_icon = transform.scale(image.load("nuss_images/sol_icon.png"), (40, 40))
+luna_icon = transform.scale(image.load("nuss_images/luna_icon.png"), (40, 40))
+ovni_icon = transform.scale(image.load("nuss_images/ovni_icon.png"), (40, 40))
+victoria = transform.scale(image.load("nuss_images/victoria.png"), (1000, 540))
 z = 0
+animacion_boss = 0
 
 while not done:
 	global timer, plus_timer, aleatorio1, aleatorio2, aleatorio3, aleatorio4, aleatorio5, aleatorio6, level, recharge
@@ -426,6 +446,8 @@ while not done:
 				level = 3
 			if evento.key == K_RIGHT:
 				level = 4
+			if evento.key == K_LEFT:
+				level = 5
 	if z == 0:
 		timer = 0
 		plus_timer = False
@@ -437,6 +459,8 @@ while not done:
 			z = 1
 	if level == 2 and z == 1:
 		fantasma = transform.scale(image.load("nuss_images/fantasma.png"), (100, 100))
+		fantasma2 = transform.scale(image.load("nuss_images/fantasma2.png"), (100, 100))
+		fantasma3 = transform.scale(image.load("nuss_images/fantasma3.png"), (100, 100))
 		background = transform.scale(image.load("nuss_images/background_sunset.png").convert(), (3480, 720))
 		plataforma.y = 310
 		plataforma.ancho = 100
@@ -447,10 +471,13 @@ while not done:
 		boss.disparo2.largo = 25
 		boss.disparo3.ancho = 25
 		boss.disparo3.largo = 25
+		barra.color = (249,192,49)
 		aleatorio3 = randrange(1,6)
 		z = 2
 	if level == 3 and z == 2:
 		fantasma = transform.scale(image.load("nuss_images/fantasma.png"), (200, 200))
+		fantasma2 = transform.scale(image.load("nuss_images/fantasma2.png"), (200, 200))
+		fantasma3 = transform.scale(image.load("nuss_images/fantasma3.png"), (200, 200))
 		background = transform.scale(image.load("nuss_images/background_night.png").convert(), (3480, 720))
 		plataforma.y = 260
 		plataforma.x = 250
@@ -470,11 +497,14 @@ while not done:
 		aleatorio4 = 1
 		aleatorio5 = 3
 		recharge = True
+		barra.color = (110,1,219)
 		if aleatorio4 != aleatorio5:
 			z = 3
 	if level == 4 and z == 3:
 		fantasma = transform.scale(image.load("nuss_images/fantasma.png"), (100, 100))
-		background = transform.scale(image.load("nuss_images/background_sunset.png").convert(), (3480, 720))
+		fantasma2 = transform.scale(image.load("nuss_images/fantasma2.png"), (100, 100))
+		fantasma3 = transform.scale(image.load("nuss_images/fantasma3.png"), (100, 100))
+		background = transform.scale(image.load("nuss_images/background_space.png").convert(), (3480, 720))
 		plataforma.y = 310
 		plataforma.x = 50
 		plataforma.ancho = 100
@@ -497,13 +527,27 @@ while not done:
 		aleatorio3 = randrange(1,6)
 		aleatorio6 = randrange(1,6)
 		recharge = True
+		barra.color = (0,228,55)
 		z = 4
+	if level == 5 and z == 4:
+		fantasma = transform.scale(image.load("nuss_images/fantasma.png"), (200, 200))
+		fantasma2 = transform.scale(image.load("nuss_images/fantasma2.png"), (200, 200))
+		fantasma3 = transform.scale(image.load("nuss_images/fantasma3.png"), (200, 200))
+		plataforma.x = 50
+		plataforma.y = 260
+		z = 5
 	x_relativa = x % 3480
-	#plataforma.Movimiento()
 	screen.blit(background, [x_relativa - 3480, 0])
 	screen.blit(background, [x_relativa, 0])
 	x -= 1
-	#boss.Disparar()
+	animacion_boss += 1
+	plataforma.Movimiento()
+	if barra.ancho >= 20:
+		boss.Disparar()
+	barra.ancho += 0.125
+	if level != 5:
+		barra.DibujarObjeto()
+		screen.blit(barra_bg, [340, 25])
 	if level == 1:
 		if aleatorio1 == 1 or aleatorio2 == 1:
 			screen.blit(rayo, [boss.disparo1.x-125, boss.disparo1.y-125])
@@ -512,6 +556,7 @@ while not done:
 		if aleatorio1 == 3 or aleatorio2 == 3:
 			screen.blit(rayo, [boss.disparo3.x-125, boss.disparo3.y-125])
 		screen.blit(nube, [700, boss.y])
+		screen.blit(nube_icon, [barra.x + barra.ancho - 40, barra.y-10])
 	if level == 2:
 		if aleatorio1 == 1 or aleatorio2 == 1 or aleatorio3 == 1:
 			screen.blit(fuego, [boss.disparo1.x-62.5, boss.disparo1.y-75])
@@ -524,45 +569,57 @@ while not done:
 		if aleatorio1 == 5 or aleatorio2 == 5 or aleatorio3 == 5:
 			screen.blit(fuego, [boss.disparo5.x-62.5, boss.disparo5.y-75])
 		screen.blit(sol, [700, boss.y])
+		screen.blit(sol_icon, [barra.x + barra.ancho - 40, barra.y-10])
 	if level == 3:
 		if aleatorio1 == 1 or aleatorio2 == 1:
-			screen.blit(meteorito, [boss.disparo1.x-125, boss.disparo1.y-125])
+			screen.blit(meteorito1, [boss.disparo1.x-125, boss.disparo1.y-125])
 		if aleatorio1 == 2 or aleatorio2 == 2:
-			screen.blit(meteorito, [boss.disparo2.x-125, boss.disparo2.y-125])
+			screen.blit(meteorito2, [boss.disparo2.x-125, boss.disparo2.y-125])
 		if aleatorio1 == 3 or aleatorio2 == 3:
-			screen.blit(meteorito, [boss.disparo3.x-125, boss.disparo3.y-125])
+			screen.blit(meteorito1, [boss.disparo3.x-125, boss.disparo3.y-125])
 		if aleatorio4 == 1 or aleatorio5 == 1:
-			screen.blit(meteorito, [boss.disparo6.x-125, boss.disparo6.y-125])
+			screen.blit(meteorito2, [boss.disparo6.x-125, boss.disparo6.y-125])
 		if aleatorio4 == 2 or aleatorio5 == 2:
-			screen.blit(meteorito, [boss.disparo7.x-125, boss.disparo7.y-125])
+			screen.blit(meteorito1, [boss.disparo7.x-125, boss.disparo7.y-125])
 		if aleatorio4 == 3 or aleatorio5 == 3:
-			screen.blit(meteorito, [boss.disparo8.x-125, boss.disparo8.y-125])
+			screen.blit(meteorito2, [boss.disparo8.x-125, boss.disparo8.y-125])
 		screen.blit(luna, [700, boss.y])
+		screen.blit(luna_icon, [barra.x + barra.ancho - 40, barra.y-10])
 	if level == 4:
 		if aleatorio1 == 1 or aleatorio2 == 1 or aleatorio3 == 1:
-			screen.blit(fuego, [boss.disparo1.x-62.5, boss.disparo1.y-75])
+			screen.blit(ovni, [boss.disparo1.x-62.5, boss.disparo1.y-75])
 		if aleatorio1 == 2 or aleatorio2 == 2 or aleatorio3 == 2:
-			screen.blit(fuego, [boss.disparo2.x-62.5, boss.disparo2.y-75])
+			screen.blit(ovni, [boss.disparo2.x-62.5, boss.disparo2.y-75])
 		if aleatorio1 == 3 or aleatorio2 == 3 or aleatorio3 == 3:
-			screen.blit(fuego, [boss.disparo3.x-62.5, boss.disparo3.y-75])
+			screen.blit(ovni, [boss.disparo3.x-62.5, boss.disparo3.y-75])
 		if aleatorio1 == 4 or aleatorio2 == 4 or aleatorio3 == 4:
-			screen.blit(fuego, [boss.disparo4.x-62.5, boss.disparo4.y-75])
+			screen.blit(ovni, [boss.disparo4.x-62.5, boss.disparo4.y-75])
 		if aleatorio1 == 5 or aleatorio2 == 5 or aleatorio3 == 5:
-			screen.blit(fuego, [boss.disparo5.x-62.5, boss.disparo5.y-75])
+			screen.blit(ovni, [boss.disparo5.x-62.5, boss.disparo5.y-75])
 		if aleatorio4 == 1 or aleatorio5 == 1 or aleatorio6 == 1:
-			screen.blit(fuego, [boss.disparo6.x-62.5, boss.disparo6.y-75])
+			screen.blit(ovni, [boss.disparo6.x-62.5, boss.disparo6.y-75])
 		if aleatorio4 == 2 or aleatorio5 == 2 or aleatorio6 == 2:
-			screen.blit(fuego, [boss.disparo7.x-62.5, boss.disparo7.y-75])
+			screen.blit(ovni, [boss.disparo7.x-62.5, boss.disparo7.y-75])
 		if aleatorio4 == 3 or aleatorio5 == 3 or aleatorio6 == 3:
-			screen.blit(fuego, [boss.disparo8.x-62.5, boss.disparo8.y-75])
+			screen.blit(ovni, [boss.disparo8.x-62.5, boss.disparo8.y-75])
 		if aleatorio4 == 4 or aleatorio5 == 4 or aleatorio6 == 4:
-			screen.blit(fuego, [boss.disparo9.x-62.5, boss.disparo9.y-75])
+			screen.blit(ovni, [boss.disparo9.x-62.5, boss.disparo9.y-75])
 		if aleatorio4 == 5 or aleatorio5 == 5 or aleatorio6 == 5:
-			screen.blit(fuego, [boss.disparo10.x-62.5, boss.disparo10.y-75])
-		screen.blit(sol, [700, boss.y])
-	plataforma.Movimiento()
-	boss.Disparar()
-	screen.blit(fantasma, [plataforma.x, plataforma.y])
+			screen.blit(ovni, [boss.disparo10.x-62.5, boss.disparo10.y-75])
+		screen.blit(alien, [700, boss.y])
+		screen.blit(ovni_icon, [barra.x + barra.ancho - 40, barra.y-10])
+	if level == 5:
+		screen.blit(victoria, [250, 100])
+		barra.ancho = 0
+	if plataforma.vidas == 3:
+		screen.blit(fantasma, [plataforma.x, plataforma.y])
+	if plataforma.vidas == 2:
+		screen.blit(fantasma2, [plataforma.x, plataforma.y])
+	if plataforma.vidas == 1:
+		screen.blit(fantasma3, [plataforma.x, plataforma.y])
+	if barra.ancho >= 600:
+		level += 1
+		barra.ancho = 0
 	if plataforma.vidas <= 0:
 		done = True
 	display.flip()
